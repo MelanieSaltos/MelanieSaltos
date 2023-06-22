c10aa2bf1a550ceb0db61509c8aa07a5ee3d05a2
MelanieSaltos <melaniesalto2003@gmail.com>
2023-06-22 18:42:29 -0500

    a

--- javascript/dom/Taller_1/script.js
+++ javascript/dom/Taller_1/script.js
@@ -1,3 +1,8 @@
 function cargar_elemento(){
    var container= document.getElementById('container')
+   var h1= document.createElement('h1')
+   var texto= document.createTextNode('Melanie Saltos')
+   h1.appendChild(texto)
+   container.appendChild(h1)
+
 }
