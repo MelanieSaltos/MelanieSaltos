032320c2c8527030e79190c49479b2bae5ad6905
MelanieSaltos <melaniesalto2003@gmail.com>
2023-06-28 21:42:24 -0500

    qq

--- javascript/dom/Taller_8/script.js
+++ javascript/dom/Taller_8/script.js
@@ -1,35 +1,11 @@
 var div_cajita= document.getElementById('cajita')
-var es_presionada=false  
-var offset=[0, 0]
 
-div_cajita.addEventListener('mousedown', function(valor){
- es_presionada=true
+setTimeout(() => {
+   div_cajita.style.left=(div_cajita.style.left + 10)+ 'px'
+   console.log("Mensaje 1")
+}, 2000);
 
- offset=[div_cajita.offsetLeft - valor.clientX, div_cajita.offsetTop - valor.clientY ]
-
-/**console.log('X: $ {valor.clientX}')
-console.log('Y: $ {valor.clientY}')
-/***otra forma */
-/**console.log('Div top:'+ div_cajita.offsetTop)
-console.log('Div left:'+ div_cajita.offsetLeft)**/
-
- console.log(es_presionada)
-}, true)
-
-div_cajita.addEventListener('mouseup', function(valor){
-   es_presionada=false
-   
-   console.log(es_presionada)
-  }, true)
-
-  div_cajita.addEventListener('mousemove', function(valor){
-   valor.preventDefault()
-   if(es_presionada){
-      div_cajita.style.left=(valor.clientX + offset[0])+'px'
-      div_cajita.style.top=(valor.clientY + offset[1])+'px'
-   }
-
-  }, true)
-
-  
- 
+setTimeout(() => {
+   div_cajita.style.left=(div_cajita.style.left + 10)+ 'px'
+   console.log("Mensaje 2")
+}, 3000);
