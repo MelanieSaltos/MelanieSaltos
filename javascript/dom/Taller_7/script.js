6afdb3fdae4efa9394439137dd4ede9c0af3478f
MelanieSaltos <melaniesalto2003@gmail.com>
2023-06-28 20:52:29 -0500

    s

--- javascript/dom/Taller_7/script.js
+++ javascript/dom/Taller_7/script.js
@@ -1,1 +1,20 @@
+var div_cajita= document.getElementById('cajita')
+var es_presionada=false  
 
+div_cajita.addEventListener('mousedown', function(valor){
+ es_presionada=true
+
+ console.log(es_presionada)
+}, true)
+
+div_cajita.addEventListener('mousedown', function(valor){
+   es_presionada=false
+   
+   console.log(es_presionada)
+  }, true)
+
+  div_cajita.addEventListener('mousemove', function(valor){
+   
+   
+   console.log('[mousemove]')
+  }, true)
