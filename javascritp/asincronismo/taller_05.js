345884f6298bfbf22d67a04be19038036a6499a2
MelanieSaltos <melaniesalto2003@gmail.com>
2023-08-10 20:06:11 -0500

    a

--- javascritp/asincronismo/taller_05.js
+++ javascritp/asincronismo/taller_05.js
@@ -1,32 +1,35 @@
-const saludar=(nombre)=>{
-return new Promise((resolve, reject) =>{
-    setTimeout(function() {
-        console.log(`Hola ${nombre}`)
-        resolve(nombre)
-    }, 1000)
+async function saludar(nombre) {
+    return new Promise((resolve, reject) => {
+        setTimeout(function() {
+            console.log(`Hola ${nombre}.`)
+            resolve(nombre)
+        }, 1000) 
     })
 }
 
-const hablar=(nombre)=>{
-    return new Promise((resolve, reject) =>{
+async function hablar(nombre) {
+    return new Promise((resolve, reject) => {
         setTimeout(function() {
-            console.log('blah,blah, blah.......')
+            console.log(`Bla, bla, bla...`)
             resolve(nombre)
-        }, 1000)
-        })
-    }
-    
-    const adios=(nombre)=>{
-        return new Promise((resolve, reject) =>{
-            setTimeout(function() {
-                console.log('Terminando conversacion')
-                resolve(nombre)
-            }, 1000)
-            })
-        }
+        }, 1000) 
+    })
+}
 
+async function adios(nombre) {
+    return new Promise((resolve, reject) => {
+        setTimeout(function() {
+            console.log(`Terminando conversacion...`)
+            resolve(nombre)
+        }, 1000) 
+    })
+}
 
-console.log('Iniciando conversacion...')
-saludar('Melanie')
-.then((dato) => hablar(dato))
-.then((dato) => adios(dato))
+async function main() {
+    console.log('Iniciando conversacion...')
+    let nombre = await saludar('Guillermo')
+    await hablar( nombre )
+    await adios( nombre ) 
+}
+
+main()
