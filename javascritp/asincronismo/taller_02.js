e1e5eb442b05243eb3f41b95530ef623645153bb
MelanieSaltos <melaniesalto2003@gmail.com>
2023-08-10 19:25:51 -0500

    sx

---
+++ javascritp/asincronismo/taller_02.js
@@ -0,0 +1,10 @@
+console.log("Primera frase")
+setTimeout(function(){
+console.log('Segunda frase')
+
+} , 1000)
+setTimeout(function(){
+console.log('Tercera frase')
+
+} , 2000)
+console.log('Cuarta Frase')
