a98e1fd28a72e20d3f21ad06c0318dfc3e4ebd1f
MelanieSaltos <melaniesalto2003@gmail.com>
2023-08-16 20:35:34 -0500

    A

--- javascritp/asincronismo/taller_07/scripts.js
+++ javascritp/asincronismo/taller_07/scripts.js
@@ -1,33 +1,63 @@
-const URL_API = 'https://rickandmortyapi.com/api/character/'
+const API = 'https://rickandmortyapi.com/api/character/'
 
 var xhttp = null
 
-function cargar_datos(personaje_id) {
+function personaje(texto) {
+    let div = document.createElement('div')
+    let h1_texto = document.createTextNode(texto.name)
+    let h1 = document.createElement('h1')
+    h1.appendChild(h1_texto)
+    div.appendChild(h1)
+    let img = document.createElement('img')
+    img.src = texto.image
+    div.appendChild(img)
+
+    let contenedor = document.getElementById('contenedor')
+    contenedor.appendChild( div )
+}
+
+function cargar_datos(url_api, fn) {
     xhttp = new XMLHttpRequest()
-    xhttp.open('GET', URL_API + personaje_id, true)
-    xhttp.send()
-    xhttp.onreadystatechange = obtener_respuesta
-}
-
-function obtener_respuesta() {
-    if (xhttp && xhttp.readyState == 4 && xhttp.status == 200) {
-        texto = JSON.parse(xhttp.responseText)
-        
-        let div = document.createElement('div')
-        let h1_texto = document.createTextNode(texto.name)
-        let h1 = document.createElement('h1')
-        h1.appendChild(h1_texto)
-        div.appendChild(h1)
-        let img = document.createElement('img')
-        img.src = texto.image
-        div.appendChild(img)
-
-        let contenedor = document.getElementById('contenedor')
-        contenedor.appendChild( div )
+    xhttp.open('GET', url_api, true)
+    xhttp.onreadystatechange = function(e) {
+        if (xhttp && xhttp.readyState == 4) {
+            if (xhttp.status == 200) {
+                fn(null, JSON.parse(xhttp.responseText))
+            } else {
+                const error = `[error]: ${url_api}`
+                fn(error, null)
+            }
+        }
     }
+    xhttp.send()
 }
 
-cargar_datos(1)
-cargar_datos(2)
-cargar_datos(3)
-cargar_datos(4)
+const invocacionSincronica = function(error, data) {
+    if (error)
+        return console.error( error )
+
+    console.log( data.info.count )
+
+    cargar_datos( API + data.results[0].id, function(error2, data2) {
+        if (error2)
+            return console.error( error2 )
+
+        personaje( data2 )
+
+        cargar_datos( API + data.results[1].id, function(error3, data3) {
+            if (error3)
+                return console.error( error3 )
+    
+            personaje( data3 )
+            
+            cargar_datos( API + data.results[2].id, function(error4, data4) {
+                if (error4)
+                    return console.error( error4 )
+        
+                personaje( data4 )
+            } )    
+        } )    
+    } )
+}
+
+cargar_datos(API, invocacionSincronica)
